55efc3babd9f795760fad411f1ab7a13c4c86cff
Ben_G <ben@sveltestudios.com>
2022-08-11 09:47:36 +0930

    changed what is returned on addProduct

--- src/Order/addProduct.ts
+++ src/Order/addProduct.ts
@@ -1,15 +1,18 @@
 import { HttpModule } from '@whppt/api-express';
 
-const addProduct: HttpModule<{ status: number }> = {
+const addProduct: HttpModule<
+  { productId: string; quantity: number; orderId?: string | undefined },
+  { _id: string; productOrder: { _id: string; quantity: number }[] }
+> = {
   authorise({ $roles }, { user }) {
     return $roles.validate(user, []);
   },
-  exec() {
+  exec({ $id }, { productId, quantity, orderId }) {
     //   events:[
     //     createdOrder,
     //     productOrderAddedToOrder
     // ]
-    return Promise.resolve({ status: 200 });
+    return Promise.resolve({ _id: orderId || $id(), productOrder: [{ _id: productId, quantity }] });
   },
 };
 
