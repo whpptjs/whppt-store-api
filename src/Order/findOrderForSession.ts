75c1ebaf42b455da20994254961e9866b8a35ba4
Ben_G <ben@sveltestudios.com>
2022-09-06 13:26:34 +0930

    adding findOrderForSession details

--- src/Order/findOrderForSession.ts
+++ src/Order/findOrderForSession.ts
@@ -1,14 +1,27 @@
 import { HttpModule } from '@whppt/api-express';
+import { Product } from 'src/Product/Models/Product';
+import { Order } from './Models/Order';
 
-const findOrderForSession: HttpModule<{ status: number }> = {
+const findOrderForSession: HttpModule<{ orderId: string }, { products: Product[] } & Order> = {
   authorise({ $roles }, { user }) {
     return $roles.validate(user, []);
   },
-  exec() {
-    // const cart = findOneFromSessionId() || findOrderNotCompletedWithMostRecentUpdatedDate()
-    // abandonOtherOrders()
-    // return cart;
-    return Promise.resolve({ status: 200 });
+  exec({ $mongo: { $db, $dbPub } }, { orderId }) {
+    const query = orderId ? { _id: orderId } : { status: { $ne: 'completed' } };
+
+    return $db
+      .collection<Order>('orders')
+      .findOne(query)
+      .then(order => {
+        const productIds = order.items.map(i => i.productId);
+        return $dbPub
+          .collection('products')
+          .find({ _id: { $in: productIds } })
+          .toArray()
+          .then(products => {
+            return { ...order, products };
+          });
+      });
   },
 };
 
