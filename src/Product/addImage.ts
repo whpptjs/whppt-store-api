c307d7959d2cefaffb7aeba13cfe3b1e53e951bc
Ben_G <ben@sveltestudios.com>
2022-10-11 17:22:36 +1030

    changed Props for add image

--- src/Product/addImage.ts
+++ src/Product/addImage.ts
@@ -1,35 +1,37 @@
 import assert from 'assert';
 import { HttpModule } from '@whppt/api-express';
 import { Product, WhpptProductImageData } from './Models/Product';
+import { WhpptImageData } from '@whppt/next';
 
 export type ChangeProductImageArgs = {
   domainId: string;
   productId: string;
   featureImageId?: string;
-  image: WhpptProductImageData;
+  image: WhpptImageData;
 };
 
-const changeImage: HttpModule<ChangeProductImageArgs, void> = {
+const changeImage: HttpModule<ChangeProductImageArgs, WhpptProductImageData> = {
   authorise({ $identity }, { user }) {
     return $identity.isUser(user);
   },
-  exec({ $database, createEvent }, { domainId, productId, image, featureImageId }) {
+  exec({ $database, createEvent, $id }, { domainId, productId, image, featureImageId }) {
     assert(domainId, 'Product requires a Domain Id.');
     assert(productId, 'Product requires a Product Id.');
 
-    assert(image._id, 'Product requires an Image.');
     assert(image.desktop.galleryItemId, 'Product requires at minimum a Desktop image.');
     return $database.then(({ document, startTransaction }) => {
       return document.fetch<Product>('products', productId).then(product => {
         assert(product, 'Product does not exsist');
-        assert(!product.images.find(i => i._id === image._id), 'Product already has an exsiting id for that image.');
-
-        const events = [createEvent('ProductImageAdded', { _id: productId, image })];
-        Object.assign(product, { domainId, productId, images: product.images ? [...product.images, image] : [image] });
+        const newImage = {
+          ...image,
+          _id: $id.newId(),
+        } as WhpptProductImageData;
+        const events = [createEvent('ProductImageAdded', { _id: productId, image: newImage })];
+        Object.assign(product, { domainId, productId, images: product.images ? [...product.images, newImage] : [newImage] });
 
         if (!product.featureImageId) {
-          events.push(createEvent('ProductFeatureImageSet', { _id: productId, featureImageId: image._id }));
-          Object.assign(product, { domainId, productId, featureImageId: image._id });
+          events.push(createEvent('ProductFeatureImageSet', { _id: productId, featureImageId: newImage._id }));
+          Object.assign(product, { domainId, productId, featureImageId: newImage._id });
         } else if (featureImageId && product.featureImageId !== featureImageId) {
           events.push(createEvent('ProductFeatureImageChanged', { _id: productId, featureImageId }));
           Object.assign(product, { domainId, productId, featureImageId });
@@ -37,7 +39,7 @@ const changeImage: HttpModule<ChangeProductImageArgs, void> = {
 
         return startTransaction(session => {
           return document.saveWithEvents('products', product, events, { session });
-        }).then(() => {});
+        }).then(() => newImage);
       });
     });
   },
